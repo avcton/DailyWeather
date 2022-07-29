30a43ea141339503914b2cd66aa4cfd67d5cc312
avcton <avcton@gmail.com>
2022-07-29 18:05:47 +0500

    Extended Weather Page Reworked

--- lib/Screens/extended_weather.dart
+++ lib/Screens/extended_weather.dart
@@ -67,36 +67,91 @@ class _Extended_Weather extends State<Extended_Weather>
                             child: Text(snapshot.error.toString(),
                                 style: Theme.of(context).textTheme.headline1));
                       } else if (snapshot.hasData) {
-                        return Container(
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(20)),
-                          height: (size.height - size.height * 30 / 100),
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 10, bottom: 10),
-                            child: ListView.builder(
-                                itemCount: snapshot.data?.length,
-                                itemBuilder: (context, index) {
-                                  return ListTile(
-                                    onTap: () {},
-                                    leading: Image.asset(
-                                      'assets/WeatherCodes/${snapshot.data?[index].weatherIcon}@2x.png',
-                                    ),
-                                    trailing: Text(
-                                        "${weekDayToString(day: snapshot.data?[index].date?.weekday)} ${DateFormat('h:mm a').format(snapshot.data?[index].date ?? DateTime.now())}",
-                                        style: GoogleFonts.bellota(
-                                            textStyle: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Theme.of(context).primaryColor,
-                                        ))),
-                                    title: Text(
-                                        "${snapshot.data?[index].weatherDescription}",
-                                        style: GoogleFonts.bellota(
-                                            textStyle: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Theme.of(context).primaryColor,
-                                        ))),
+                        final sortedList = sortWeatherDays(snapshot.data ?? []);
+
+                        return Padding(
+                          padding: const EdgeInsets.only(top: 50.0),
+                          child: SizedBox(
+                            height: size.height - size.height * 40 / 100,
+                            child: ListView.separated(
+                                separatorBuilder: ((context, index) {
+                                  return const SizedBox(
+                                    width: 10,
                                   );
+                                }),
+                                scrollDirection: Axis.horizontal,
+                                itemCount: sortedList.length,
+                                itemBuilder: (context, index) {
+                                  return Container(
+                                      width: size.width - size.width * 8 / 100,
+                                      decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(top: 25.0),
+                                        child: Column(children: [
+                                          Center(
+                                            child: Text(
+                                                weekDayToString(
+                                                    day: sortedList[index][0]
+                                                        .date!
+                                                        .weekday),
+                                                style: GoogleFonts.bellota(
+                                                    fontSize: size.width -
+                                                        size.width * 90 / 100,
+                                                    textStyle: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: Theme.of(context)
+                                                          .primaryColor,
+                                                    ))),
+                                          ),
+                                          Expanded(
+                                            child: ListView.builder(
+                                                itemCount:
+                                                    sortedList[index].length,
+                                                itemBuilder: (context, indx) {
+                                                  return ListTile(
+                                                    onTap: () {},
+                                                    leading: Image.asset(
+                                                      'assets/WeatherCodes/${sortedList[index][indx].weatherIcon}@2x.png',
+                                                    ),
+                                                    trailing: Text(
+                                                        DateFormat('h:mm a')
+                                                            .format(sortedList[
+                                                                            index]
+                                                                        [indx]
+                                                                    .date ??
+                                                                DateTime.now()),
+                                                        style:
+                                                            GoogleFonts.bellota(
+                                                                textStyle:
+                                                                    TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          color:
+                                                              Theme.of(context)
+                                                                  .primaryColor,
+                                                        ))),
+                                                    title: Text(
+                                                        "${sortedList[index][indx].weatherDescription}",
+                                                        style:
+                                                            GoogleFonts.bellota(
+                                                                textStyle:
+                                                                    TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          color:
+                                                              Theme.of(context)
+                                                                  .primaryColor,
+                                                        ))),
+                                                  );
+                                                }),
+                                          ),
+                                        ]),
+                                      ));
                                 }),
                           ),
                         );
@@ -134,5 +189,63 @@ class _Extended_Weather extends State<Extended_Weather>
         return "Sunday";
     }
     return "null";
+  }
+
+  List<List<Weather>> sortWeatherDays(List<Weather> list) {
+    int currentDay = list[0].date!.weekday;
+
+    List<List<Weather>> sortedList = [];
+    List<Weather> Monday = [];
+    List<Weather> Tuesday = [];
+    List<Weather> Wednesday = [];
+    List<Weather> Thursday = [];
+    List<Weather> Friday = [];
+    List<Weather> Saturday = [];
+    List<Weather> Sunday = [];
+
+    Map<int, List<Weather>> dayMap = {
+      1: Monday,
+      2: Tuesday,
+      3: Wednesday,
+      4: Thursday,
+      5: Friday,
+      6: Saturday,
+      7: Sunday,
+    };
+
+    for (var weather in list) {
+      if (weather.date!.weekday == 1) {
+        Monday.add(weather);
+      }
+      if (weather.date!.weekday == 2) {
+        Tuesday.add(weather);
+      }
+      if (weather.date!.weekday == 3) {
+        Wednesday.add(weather);
+      }
+      if (weather.date!.weekday == 4) {
+        Thursday.add(weather);
+      }
+      if (weather.date!.weekday == 5) {
+        Friday.add(weather);
+      }
+      if (weather.date!.weekday == 6) {
+        Saturday.add(weather);
+      }
+      if (weather.date!.weekday == 7) {
+        Sunday.add(weather);
+      }
+    }
+
+    for (int i = 0; i < 7; i++, currentDay++) {
+      if (currentDay > 7) {
+        currentDay = 1;
+      }
+      if (dayMap[currentDay]!.isNotEmpty) {
+        sortedList.add(dayMap[currentDay] ?? []);
+      }
+    }
+
+    return sortedList;
   }
 }
